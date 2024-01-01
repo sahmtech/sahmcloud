85fdc8bcaa29c4b603162f61d3ac49b79c89b66a
Victor-Ahmad <m.ahmad@sahmtech.com>
2024-01-01 05:23:22 +0300

    Initial commit

---
+++ lang/ar/receipt.php
@@ -0,0 +1,17 @@
+<?php 
+ return [ 
+"product" => "الصنف",
+"qty" => "الكمية",
+"unit_price" => "سعر الوحدة",
+"subtotal" => " إجمالي",
+"discount" => "الخصم",
+"tax" => "الضريبة",
+"total" => "الإجمالي",
+"invoice_number" => "رقم الفاتورة",
+"date" => "التاريخ",
+"receipt_settings" => "إعدادات الفاتورة",
+"receipt_settings_mgs" => "كل الإعدادات المتعلقة بالفواتير لهذا الفرع",
+"print_receipt_on_invoice" => "طباعة الفاتورة تلقائيا بعد إنهاء البيع",
+"receipt_printer_type" => "نوع طابعة الفواتير",
+"receipt_settings_updated" => "تم تحديث إعدادات الفاتورة",
+];
---
+++ lang/ar/stock_adjustment.php
@@ -0,0 +1,19 @@
+<?php 
+ return [ 
+"stock_adjustment" => "المخزون التالف",
+"stock_adjustments" => "المخزون التالف",
+"list" => "قائمة المخزون التالف",
+"add" => "إضافة تالف",
+"all_stock_adjustments" => "كل المخزون التالف",
+"search_product" => "البحث عن الأصناف",
+"adjustment_type" => "نوع الإتلاف",
+"normal" => "عادي",
+"abnormal" => "غير عادي",
+"total_amount" => "الإجمالي",
+"total_amount_recovered" => "الإجمالي المسترد",
+"reason_for_stock_adjustment" => "السبب",
+"stock_adjustment_added_successfully" => "تمت إضافة مخزون تالف بنجاح",
+"search_products" => "البحث عن الأصناف",
+"delete_success" => "تم حذف المخزون التالف بنجاح",
+"view_details" => "عرض تفاصيل المخزون التالف",
+];
