051c197dce09322a26deca6b7cfe5ae371b7bab7
MHD_Amen <mohammadamen042@gmail.com>
2024-02-28 00:26:13 +0300

    ..

--- database/migrations/2024_02_28_001323_test.php
+++ database/migrations/2024_02_28_001323_test.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('test', function (Blueprint $table) {
+        Schema::create('test', function (Blueprint $table) {
             $table->bigIncrements('id');
 
         });
